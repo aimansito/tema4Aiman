17e0f375125d9f1f25d24a10d1c0f7b979680dc9
ahardao1001 <ahardao1001@gmail.com>
2023-11-10 09:11:50 +0100

    añade metodo y constructores

--- src/main/java/clases/Vehiculo.java
+++ src/main/java/clases/Vehiculo.java
@@ -8,6 +8,21 @@ package clases;
  *
  * @author aiman
  */
+//definicion de la clase(static,extends,abstract,etc)
 public class Vehiculo {
+    //atributos --caracteristicas  
+    String matricula;
+    String marca;
+    double velocidad;
     
+    
+    //metodos --acciones
+    //metodo constructor ,se llama igual que la clase siempre es publico 
+    //no devuelve nada, puede tener parametros
+    
+    public Vehiculo(String matricula,String marca,double velocidad){
+        this.matricula = matricula; 
+        this.marca = marca;
+        this.velocidad = velocidad;
+    }
 }
